060fd532ad567c55c6827fb2f7695d0a4b0f5c5e
Rodrigo Cipriani da Rosa <chopsss@gmail.com>
2019-03-03 16:37:52 -0300

    Updates

---
+++ precache-manifest.6eea9873d58f68c8012e373c8172c14d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "dbc9b660f08b6db1f022",
+    "url": "/notes/static/js/main.dbc9b660.chunk.js"
+  },
+  {
+    "revision": "f0f7e8dad26697437fb7",
+    "url": "/notes/static/js/1.f0f7e8da.chunk.js"
+  },
+  {
+    "revision": "dbc9b660f08b6db1f022",
+    "url": "/notes/static/css/main.c81d3353.chunk.css"
+  },
+  {
+    "revision": "38049f89173fcae9bf879689aa95c1cc",
+    "url": "/notes/index.html"
+  }
+];
